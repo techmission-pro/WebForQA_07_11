2ec488f10ee323d51432bc0dcdb58ae83616c026
sstarostenko17 <sstarostenko17@gmail.com>
2022-12-04 09:54:59 +0200

    calc.3

--- resources/calc.js
+++ resources/calc.js
@@ -21,7 +21,7 @@ document.querySelector('.calc-calculate').addEventListener('clic', function() {
 	 
      const sum = num1 + num2;
 
-     document.querySelector('.calc-result') value=sum;
+     document.querySelector('.calc-result').value = result;
 });
 
 
