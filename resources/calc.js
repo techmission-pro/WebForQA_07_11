7f12af72b6247e3cfcd34cf63533ff6bff79f6c3
LyubaSuptsarel <lyubatutaeva@gmail.com>
2022-12-07 13:22:18 +0200

    add calculator

---
+++ resources/calc.js
@@ -0,0 +1,9 @@
+document.querySelector('.calc_calculate') .addEventListener('click', function () {
+    const num1 = +document.querySelector('[name="num1"]').value;
+    const num2 = +document.querySelector('[name="num2"]').value;
+
+    const sum = num1 + num2;
+
+    document.querySelector('.calc_result').value = sum;
+})
+
